c200386322ce20bc1d278f8c7a85acb443e58050
Bas Kooijman <bas.kooijman@vu.nl>
2022-02-26 11:41:50 +0100

    Update predict_OECD202_dichromate.m

--- OECD202_dichromate/predict_OECD202_dichromate.m
+++ OECD202_dichromate/predict_OECD202_dichromate.m
@@ -13,7 +13,7 @@ function [prdData, info] = predict_OECD202_dichromate(par, data, auxData)
   
   % initialize state vector; catenate to avoid loops
   X0 = [N_0 * ones(nc,1); % #, number of surviving individuals
-        L_0 * ones(nc,1);  % cm, structural length 
+        L_0 * ones(nc,1); % cm, structural length 
         zeros(nc,1)];     % mg/l, scaled internal concentration 
   % "scaled" here means internal concentration in the units of external ones
 
@@ -28,7 +28,7 @@ end
 function dX = dNLc(~, X, C, nc, c_0, b, k_e, h_0, k_M, g, L_m)
   % unpack state vector
   N = X(1:nc);        % nuber of surviving individuals 
-  L = X(nc+(1:nc)); % structural length
+  L = X(nc+(1:nc));   % structural length
   c = X(2*nc+(1:nc)); % scaled internal concentration
   
   dN = - N .* (h_0 + b * max(0,(c - c_0))); % #/d, change in # of surviving individuals
